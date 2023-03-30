5a15756c52fc1f483f2792ce57b43ad5ee832ae2
Siva Kumar Gampala <sivakumarg2009@gmail.com>
2023-03-30 16:56:00 +0530

    Update FactorialServiceController.java
    rest controller

--- src/main/java/com/poc/factorial/FactorialServiceController.java
+++ src/main/java/com/poc/factorial/FactorialServiceController.java
@@ -2,11 +2,13 @@ package com.poc.factorial;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class FactorialServiceController {
 	
 	@GetMapping("/factorial/{inputNum}")
-	public int cubeOfNum(@PathVariable int inputNum)
+	public int factorialOfNum(@PathVariable int inputNum)
 	{
 		
 		System.out.println("Received number is " + inputNum);
