9ac465baa1182102971d3a1f5d1a0158bf28cd79
Siva Kumar Gampala <sivakumarg2009@gmail.com>
2023-04-03 15:19:10 +0530

    Update FactorialServiceController.java
    cross

--- src/main/java/com/poc/factorial/FactorialServiceController.java
+++ src/main/java/com/poc/factorial/FactorialServiceController.java
@@ -1,10 +1,12 @@
 package com.poc.factorial;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin
 public class FactorialServiceController {
 	
 	@GetMapping("/factorial/{inputNum}")
